a84b802785d540c51c901e8538a5586b006e5f9a
Tom Chengich <tomcengich@gmail.com>
2019-10-05 22:46:02 +0300

    Basic App Set Up.

--- TicTacToeTom/MainActivity.cs
+++ TicTacToeTom/MainActivity.cs
@@ -10,6 +10,7 @@ namespace TicTacToeTom
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        //Tic Tac Tom
         private Button[] buttons;
         private int count;
         private char[,] board;
